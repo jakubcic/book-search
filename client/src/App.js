386f0db35f159115db1bf5be8c78a0cd60cc4901
Jakub Cichon <jakub@uconn.edu>
2023-03-09 00:30:02 -0500

    add graphql endpoint, ApolloClient and ApolloProvider to App

--- client/src/App.js
+++ client/src/App.js
@@ -3,29 +3,48 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+// graphql endpoint
+const httpLink = createHttpLink({
+	uri: '/graphql',
+});
+
+// middleware to attach token to every request
+const authLink = setContext((_, { headers }) => {
+	// get token from local storage
+	const token = localStorage.getItem('id_token');
+	// return headers with bearer token
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : '',
+		},
+	};
+});
+
+const client = new ApolloClient({
+	// set up ApolloClient with authorization middleware
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache(),
+});
 
 function App() {
-  return (
-    <Router>
-      <>
-        <Navbar />
-        <Routes>
-          <Route 
-            path='/' 
-            element={<SearchBooks />} 
-          />
-          <Route 
-            path='/saved' 
-            element={<SavedBooks />} 
-          />
-          <Route 
-            path='*'
-            element={<h1 className='display-2'>Wrong page!</h1>}
-          />
-        </Routes>
-      </>
-    </Router>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<Router>
+				<>
+					<Navbar />
+					<Routes>
+						<Route path='/' element={<SearchBooks />} />
+						<Route path='/saved' element={<SavedBooks />} />
+						<Route path='*' element={<h1 className='display-2'>Wrong page!</h1>} />
+					</Routes>
+				</>
+			</Router>
+		</ApolloProvider>
+	);
 }
 
 export default App;
